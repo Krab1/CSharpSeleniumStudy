fe160fd4ea4e8f6d3778cf5977f8b6e678a041bb
Nikolay Ashroev <kolya-200587@mail.ru>
2018-11-07 16:26:46 +0300

    Fix task 7

--- SeleniumDay2tests/Day4/Task7.cs
+++ SeleniumDay2tests/Day4/Task7.cs
@@ -35,11 +35,15 @@ namespace SeleniumTests
             var submenu = driver.FindElements(By.XPath($"//*[@id='app-'and contains(., '{name}')]/ul/li/a/*")).Select(a => a.Text).ToList();
             if(submenu.Count > 0)
                 submenu.ForEach(x => ClickOnSubMenu(x));
+            else
+                Assert.IsTrue(driver.FindElements(By.XPath($"//h1[contains(.,'{TrimName(name)}')]")).Count > 0, $"Не найден заголовок с именем {name}");
         }
         public void ClickOnSubMenu(string nameofsub)
         {
-            var elem = driver.FindElement(By.XPath($"//*[@id='app-']/ul/*[contains(., '{TrimName(nameofsub)}')]"));
+            var name = TrimName(nameofsub);
+            var elem = driver.FindElement(By.XPath($"//*[@id='app-']/ul/*[contains(., '{name}')]"));
             elem.Click();
+            Assert.IsTrue(driver.FindElements(By.XPath($"//h1[contains(.,'{name}')]")).Count > 0, $"Не найден заголовок с именем {name}");
         }
         private string TrimName(string originalname)
         {
